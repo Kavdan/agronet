284d589836b749b0e6c48549c6553cf0feacdf3d
Kavdan <cipinain0@gmail.com>
2025-05-22 20:41:19 +0300

    a

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,32 +1,32 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-  }
-);
+// const db = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: "postgres",
+//   }
+// );
 
 
-// const db = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: "postgres",
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//     pool: {
-//       max: 5,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 5000,
-//     },
-//   },
-// });
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 5000,
+    },
+  },
+});
 
 
 module.exports = db;
